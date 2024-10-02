15c53257302b3dfcd0c71218d3169d4516250b1b
sulaimanabid <sulaimanabid3@gmail.com>
2024-10-02 16:54:29 +0500

    build issue fixed

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -6,7 +6,7 @@ export default function About() {
           Welcome to our website! We are dedicated to providing you with the best content and resources available. Our mission is to share valuable insights, stories, and information to help you navigate through various topics.
           <br />
           <br />
-          Here, you will find a collection of blogs, articles, and resources that cover a wide range of subjects. Whether you're looking for tips, tutorials, or in-depth discussions, we strive to ensure you have the information you need at your fingertips.
+          Here, you will find a collection of blogs, articles, and resources that cover a wide range of subjects. Whether you&apos;re looking for tips, tutorials, or in-depth discussions, we strive to ensure you have the information you need at your fingertips.
           <br />
           <br />
           Thank you for visiting, and we hope you enjoy your time exploring our content!
